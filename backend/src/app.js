9b8a58c7e581184896bf6df4c48dec8f5c97d50b
Anshal543 <sherhasan543@gmail.com>
2024-05-28 19:34:48 +0500

    database connec

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// import routes
+// import userRouter from "./routes/user.route.js"
+
+
+// router declaration
+// app.use("/api/v1/users", userRouter)
+
+//http://localhost:3000/api/v1/users/register
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+
+
+export { app }
